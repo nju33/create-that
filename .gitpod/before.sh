034a29ef4bb3d9d0f693d35d787ac5b9a9cd579e
純 <nju33.ki@gmail.com>
2021-04-20 00:40:14 +0900

    Initial commit

---
+++ .gitpod/before.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -eux
+
+init_gpg() {
+  set +u
+  if [ -n "$GPG_SECRET_KEY_NJU33" ]; then
+    gpg --batch --import --allow-secret-key-import <(echo "$GPG_SECRET_KEY_NJU33" | base64 --decode)
+  fi
+  set -u
+}
+
+init_git_around_gpg() {
+  git config --global user.signingkey "$GIT_USER_SIGNINGKEY"
+  git config --global commit.gpgsign true
+  git config --global tag.gpgsign true
+}
+
+init_gpg
+init_git_around_gpg
