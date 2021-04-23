0ea937d5fd159893cee8c6bb560a67aadfbc9527
純 <nju33.ki@gmail.com>
2021-04-23 08:54:39 +0000

    build: update gitpod config according to the template

--- .gitpod/before.sh
+++ .gitpod/before.sh
@@ -3,15 +3,17 @@
 set -eux
 
 init_gpg() {
-  set +u
+  set +ux
   if [ -n "$GPG_SECRET_KEY_NJU33" ]; then
     gpg --batch --import --allow-secret-key-import <(echo "$GPG_SECRET_KEY_NJU33" | base64 --decode)
   fi
-  set -u
+  set -ux
 }
 
 init_git_around_gpg() {
+  set +ux
   git config --global user.signingkey "$GIT_USER_SIGNINGKEY"
+  set -ux
   git config --global commit.gpgsign true
   git config --global tag.gpgsign true
 }
@@ -21,7 +23,7 @@ init_zoxide() {
     if [ ! -d "$HOME/.local/share/zoxide" ]; then
       mkdir -p "$HOME/.local/share/zoxide"
     fi
-    
+
     cp "$GITPOD_REPO_ROOT/.gitpod/zoxide/db.zo" "$HOME/.local/share/zoxide/"
   fi
 }
---
+++ .gitpod/command.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -eux
+
+init_git() {
+  git config --global include.path "$HOME/.dotfiles/.gitconfig"
+}
+
+init_ngrok() {
+  set +ux
+  if [ -n "$NGROK_AUTHTOKEN" ]; then
+    ngrok authtoken "$NGROK_AUTHTOKEN"
+  fi
+  set -ux
+}
+
+init_gh() {
+  set +ux
+  if [ -n "$GH_AUTHTOKEN" ]; then
+    gh auth login --with-token < <(echo "$GH_AUTHTOKEN")
+  fi
+  set -ux
+}
+
+init_git
+init_ngrok
+init_gh
+
+echo 'Please execute following like'
+echo '  init_code'
